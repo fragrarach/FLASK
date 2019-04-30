981431c6c86b911d81d75fd954c544832f6854c2
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-04-30 11:30:51 -0400

    Implemented searching (elasticsearch)

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from elasticsearch import Elasticsearch
 
 
 db = SQLAlchemy()
@@ -34,6 +35,7 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.config['ELASTICSEARCH_URL'] else None
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
